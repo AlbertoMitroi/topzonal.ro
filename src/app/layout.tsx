57ef695e2ea80e09e5b643876a409b1e9025f9ba
Alberto Mitroi <Alberto.Mitroi@encora.com>
2025-10-27 17:00:45 +0200

    add plausible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { roRO } from "@clerk/localizations";
 import "./globals.css";
 import TRPCProvider from "@/lib/trpc/Provider";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,25 @@ export default function RootLayout({
   return (
     <ClerkProvider localization={roRO}>
       <html lang="ro">
+        <head>
+          {/* Script-ul Plausible */}
+          <Script
+            async
+            src="https://plausible.io/js/pa-geZ21iAX90eARs7sNf7ac.js"
+            strategy="afterInteractive"
+          />
+          <Script id="plausible-init" strategy="afterInteractive">
+            {`
+            window.plausible = window.plausible || function() {
+              (plausible.q = plausible.q || []).push(arguments)
+            };
+            plausible.init = plausible.init || function(i) {
+              plausible.o = i || {};
+            };
+            plausible.init();
+          `}
+          </Script>
+        </head>
         <body className={inter.className}>
           <TRPCProvider>{children}</TRPCProvider>
         </body>
